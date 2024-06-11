847519f67650affbfb6112e8f032afe6c7085702
Joshua Thomas-Houlker <joshua.thomashoulker@hivehome.com>
2024-06-11 21:51:48 +0100

    init

---
+++ api/src/main.rs
@@ -0,0 +1,20 @@
+mod users;
+mod food;
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+        .bind("127.0.1:8080")?
+        .run()
+        .await
+}
