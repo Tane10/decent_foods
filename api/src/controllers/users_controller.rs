9de050994a3a508e25c081a92e48f15112c14033
Tane10 <tane2@hotmail.co.uk>
2024-06-14 21:59:15 +0100

    switching to python

--- api/src/controllers/users_controller.rs
+++
@@ -1,5 +0,0 @@
-use crate::models::types::User;
-
-pub async fn get_users() -> Vec<User> {
-    user_service::fetch_users().await
-}
--- api/src/routes/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod users;
-pub mod products;
-pub mod transactions;
